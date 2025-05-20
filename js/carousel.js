3ed7bc6630f30d4111c11abd1ef348376084fc78
MiguellArao <meusprodutos008@gmail.com>
2025-05-20 19:01:19 -0300

    teste do carousel slider
    vyuyuf

--- js/carousel.js
+++ js/carousel.js
@@ -22,3 +22,18 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+// Carousel Automatico
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+document.getElementById('radio1').checked = true
+setInterval(() => {
+    proximaImg()
+}, 5000)
+function proximaImg(){
+    cont++
+    if(cont > 3){
+        cont = 1 
+    }
+    document.getElementById('radio'+cont).checked = true
+}
